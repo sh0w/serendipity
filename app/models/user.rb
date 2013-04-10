3a21706cd898d184295b17925658961895cd1e1d
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2013-04-10 17:19:32 +0200

    sign in with login (username or email)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,27 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :login
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :login
   # attr_accessible :title, :body
 
   validates_presence_of :email
+
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+### This is the correct method you override with the code above
+### def self.find_for_database_authentication(warden_conditions)
+### end
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130410143145) do
+ActiveRecord::Schema.define(:version => 20130410150808) do
 
   create_table "photos", :force => true do |t|
     t.string   "url"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20130410143145) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
