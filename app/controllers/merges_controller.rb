a0054f821a84312d3cb8a34cdeb94017e1b9b997
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2013-05-08 15:42:02 +0200

    view details of a merged photo (see original images and their users)

--- app/controllers/merges_controller.rb
+++ app/controllers/merges_controller.rb
@@ -2,6 +2,8 @@ class MergesController < ApplicationController
 
   def show
     @merge = Merge.find(params[:id])
+    @first_image = Photo.find(@merge.first_image)
+    @second_image = Photo.find(@merge.second_image)
   end
 
   def index
