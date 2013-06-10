f6ceb572b6c60365c6deb3d42dc0e9d7d603f434
Sarah Howorka <showorka.mmt-b2011@fh-salzburg.ac.at>
2013-06-10 17:10:28 +0200

    show a user's merged images on profile page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,11 @@
 class UserController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @photos = @user.photos
+    puts @photos.inspect
+    @merges = Array.new
+    @photos.each do |photo|
+      @merges.push Merge.where("first_image = ? OR second_image = ? ", photo.id, photo.id)
+    end
   end
 end
